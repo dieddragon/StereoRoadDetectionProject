252b0976ce10f84148c48e372f241cb7e02658ac
dieddragon <bugraozutemiz@gmail.com>
2014-03-19 22:56:40 +0200

    Comparison software
    Addition of the comparison software for ground truth versus algorithm
    result comparison

---
+++ GT vs Labeled Comparison/RoadDetectionResulting/RoadDetectionResulting/GUI.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace RoadDetectionResulting
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbGT = new System.Windows.Forms.PictureBox();
+            this.pbResult = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.rtbResults = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGT)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbGT
+            // 
+            this.pbGT.Location = new System.Drawing.Point(12, 12);
+            this.pbGT.Name = "pbGT";
+            this.pbGT.Size = new System.Drawing.Size(320, 240);
+            this.pbGT.TabIndex = 0;
+            this.pbGT.TabStop = false;
+            // 
+            // pbResult
+            // 
+            this.pbResult.Location = new System.Drawing.Point(12, 258);
+            this.pbResult.Name = "pbResult";
+            this.pbResult.Size = new System.Drawing.Size(320, 240);
+            this.pbResult.TabIndex = 1;
+            this.pbResult.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(338, 475);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // rtbResults
+            // 
+            this.rtbResults.Location = new System.Drawing.Point(338, 12);
+            this.rtbResults.Name = "rtbResults";
+            this.rtbResults.Size = new System.Drawing.Size(409, 457);
+            this.rtbResults.TabIndex = 3;
+            this.rtbResults.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "\"txt files (*.txt)|*.txt|All files (*.*)|*.*\"";
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 514);
+            this.Controls.Add(this.rtbResults);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pbResult);
+            this.Controls.Add(this.pbGT);
+            this.Name = "GUI";
+            this.Text = "Road Detection Result Comparison Program";
+            ((System.ComponentModel.ISupportInitialize)(this.pbGT)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbGT;
+        private System.Windows.Forms.PictureBox pbResult;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox rtbResults;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ GT vs Labeled Comparison/RoadDetectionResulting/RoadDetectionResulting/GUI.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RoadDetectionResulting
+{
+    public partial class GUI : Form
+    {
+        String extFile = ".txt";
+        String nameGTFile;
+        String nameGTFileOld;
+
+        const int N_DATA = 359;
+
+        public GUI()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            DialogResult resultGT = System.Windows.Forms.DialogResult.Abort, resultRes = System.Windows.Forms.DialogResult.Abort;
+            if (nameGTFile == null)
+            {
+                openFileDialog1.Title = "Select the first ground truth file";
+                resultGT = openFileDialog1.ShowDialog();
+                nameGTFile = openFileDialog1.FileName;
+                nameGTFileOld = openFileDialog1.FileName;
+            }
+            nameGTFile = nameGTFileOld;
+            openFileDialog1.Title = "Select the first result file";
+            resultRes = openFileDialog1.ShowDialog();
+            String nameResultFile = openFileDialog1.FileName;
+            
+            if (resultRes == System.Windows.Forms.DialogResult.OK)
+            {
+                rtbResults.Text += nameResultFile + "\n";
+
+                for (int i = 0; i < N_DATA; i++)
+                {
+                    String[] dummyGT = nameGTFile.Split('(');
+                    String[] dummyResult = nameResultFile.Split('(');
+
+                    nameGTFile = dummyGT[0] + "(" + (i + 1).ToString() + ")" + extFile;
+                    nameResultFile = dummyResult[0] + "(" + (i + 1).ToString() + ")" + extFile;
+
+                    // Read the file line by line.
+                    System.IO.StreamReader fileGT = new System.IO.StreamReader(nameGTFile);
+                    System.IO.StreamReader fileResult = new System.IO.StreamReader(nameResultFile);
+
+                    String lineGT, lineResult;
+                    char separator = ' ';
+                    int truePos = 0, trueNeg = 0, falsePos = 0, falseNeg = 0;
+                    while ((lineGT = fileGT.ReadLine()) != null && (lineResult = fileResult.ReadLine()) != null)
+                    {
+                        String[] wordsGT = lineGT.Split(separator);
+                        String[] wordsResult = lineResult.Split(separator);
+
+                        int classGT, classResult;
+                        int rowGT, rowResult, colGT, colResult;
+
+                        int.TryParse(wordsGT[0], out rowGT);
+                        int.TryParse(wordsResult[0], out rowResult);
+
+                        int.TryParse(wordsGT[1], out colGT);
+                        int.TryParse(wordsResult[1], out colResult);
+
+                        int.TryParse(wordsGT[2], out classGT);
+                        int.TryParse(wordsResult[2], out classResult);
+
+                        //akifin yedigi boku duzeltmece
+                        if (colGT == 640)
+                        {
+                            lineGT = fileGT.ReadLine();
+                            wordsGT = lineGT.Split(separator);
+
+                            int.TryParse(wordsGT[0], out rowGT);
+                            int.TryParse(wordsGT[1], out colGT);
+                            int.TryParse(wordsGT[2], out classGT);
+                        }
+
+                        if (classGT == 1)
+                        {
+                            if (classResult == 1)
+                                truePos++;
+                            else
+                                falseNeg++;
+                        }
+                        else
+                        {
+                            if (classResult == 1)
+                                falsePos++;
+                            else
+                                trueNeg++;
+                        }
+                    }
+                    fileGT.Close();
+                    fileResult.Close();
+
+                    //The order of writing is: TP, TN, FP, FN
+                    rtbResults.Text += truePos.ToString() + " ";
+                    rtbResults.Text += trueNeg.ToString() + " ";
+                    rtbResults.Text += falsePos.ToString() + " ";
+                    rtbResults.Text += falseNeg.ToString() + "\n";
+                }
+            }
+        }
+    }
+}
