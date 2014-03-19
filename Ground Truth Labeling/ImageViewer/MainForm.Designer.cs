0f2b25c9b451ace86e295bcfcdfd8ed96ac7b2c0
dieddragon <bugraozutemiz@gmail.com>
2014-03-19 22:54:13 +0200

    Addition of ground truth labeling software

---
+++ Ground Truth Labeling/ImageViewer/MainForm.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace ImageViewer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox = new AForge.Controls.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.propertyGrid = new System.Windows.Forms.PropertyGrid();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.polygonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainMenuStrip.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "All files (*.*)|*.*|Flexible Image Transport System images (*.fit, *.fits)|*.fit;" +
+                "*.fits|PNM (Portable Any Map) images (*.pbm, *.pgm, *.ppm, *.pnm)|*.pbm;*.pgm;*." +
+                "ppm;*.pnm";
+            this.openFileDialog.Title = "Select image file";
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(964, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.polygonToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(143, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Location = new System.Drawing.Point(0, 516);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(964, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.listBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Panel2.Controls.Add(this.propertyGrid);
+            this.splitContainer1.Size = new System.Drawing.Size(964, 492);
+            this.splitContainer1.SplitterDistance = 654;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Image = null;
+            this.pictureBox.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseClick);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(11, 277);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(283, 121);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Polygon";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // propertyGrid
+            // 
+            this.propertyGrid.Enabled = false;
+            this.propertyGrid.HelpVisible = false;
+            this.propertyGrid.Location = new System.Drawing.Point(11, 10);
+            this.propertyGrid.Name = "propertyGrid";
+            this.propertyGrid.Size = new System.Drawing.Size(283, 195);
+            this.propertyGrid.TabIndex = 0;
+            this.propertyGrid.ToolbarVisible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 451);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(173, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save To File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // polygonToolStripMenuItem
+            // 
+            this.polygonToolStripMenuItem.Name = "polygonToolStripMenuItem";
+            this.polygonToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.polygonToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.polygonToolStripMenuItem.Text = "Polygon";
+            this.polygonToolStripMenuItem.Click += new System.EventHandler(this.polygonToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 538);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "MainForm";
+            this.Text = "Path Labeler - Akif Hacinecipoglu & Bugra Ozutemiz - METU ME";
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PropertyGrid propertyGrid;
+        private AForge.Controls.PictureBox pictureBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem polygonToolStripMenuItem;
+    }
+}
+
---
+++ Ground Truth Labeling/ImageViewer/MainForm.cs
@@ -0,0 +1,208 @@
+﻿// Image Viewer sample application
+// AForge.NET framework
+// http://www.aforgenet.com/framework/
+//
+// Copyright © AForge.NET, 2006-2011
+// contacts@aforgenet.com
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+using AForge.Imaging;
+using AForge.Imaging.Formats;
+using AForge.Imaging.Filters;
+
+namespace ImageViewer
+{
+    // Main form's class
+    public partial class MainForm : Form
+    {
+        string fileName, filePath;
+        // Class constructor
+        public MainForm( )
+        {
+            InitializeComponent( );
+        }
+
+        // Exit from application
+        private void exitToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            Application.Exit( );
+        }
+
+        // Open image file
+        private void openToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            openFileDialog.Filter = "PPM File | *.ppm";
+            if ( openFileDialog.ShowDialog( ) == DialogResult.OK )
+            {
+                try
+                {
+                    ImageInfo imageInfo = null;
+                    // create grayscale filter (BT709)
+                    Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
+                    // apply the filter
+                    Bitmap grayImage = filter.Apply(ImageDecoder.DecodeFromFile( openFileDialog.FileName, out imageInfo ));
+                    fileName = openFileDialog.FileName;
+                    pictureBox.Image = grayImage;
+
+                    
+                    propertyGrid.SelectedObject = imageInfo;
+                    //mageInfo.BitsPerPixel
+                    propertyGrid.ExpandAllGridItems( );
+                }
+                catch ( NotSupportedException ex )
+                {
+                    MessageBox.Show( "Image format is not supported: " + ex.Message, "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error );
+                }
+                catch ( ArgumentException ex )
+                {
+                    MessageBox.Show( "Invalid image: " + ex.Message, "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error );
+                }
+                catch
+                {
+                    MessageBox.Show( "Failed loading the image", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error );
+                }
+            }
+        }
+
+
+        int drawPoly = 0;
+        List<AForge.IntPoint> polyPoints;
+        string resultTxt;
+        List<string> resultList;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (drawPoly == 0)
+            {
+                ImageInfo imageInfo = null;
+                polyPoints = new List<AForge.IntPoint>();
+                drawPoly = 1;
+                button1.Text = "End Polygon";
+                
+
+                Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
+                // apply the filter
+                Bitmap grayImage = filter.Apply(ImageDecoder.DecodeFromFile(fileName, out imageInfo));
+                pictureBox.Image = grayImage;
+            }
+            else
+            {
+                drawPoly = 0;
+                button1.Text = "Start Polygon";
+                // sample 2 - converting .NET image into unmanaged
+                Bitmap grayImage = (Bitmap)pictureBox.Image;
+                UnmanagedImage unmanagedImage = UnmanagedImage.FromManagedImage(grayImage);
+
+                Drawing.Polygon(unmanagedImage, polyPoints, Color.White);
+                resultTxt = "";
+                resultList = new List<string>();
+                //pictureBox.Image = null;
+                
+                for (int y = 0; y < grayImage.Height; y++)
+                {
+                    for (int x = 0; x < grayImage.Width; x++)
+                    {
+                        if (IsPointInPolygon(x, y, polyPoints))
+                        {
+                            unmanagedImage.SetPixel(x, y, Color.White);
+                            resultList.Add(y.ToString() + " " + x.ToString() + " " + "1");//resultTxt += y.ToString() + " " + x.ToString() + " " + "1" + System.Environment.NewLine;
+                        }
+                        else
+                        {
+                            //unmanagedImage.SetPixel(x, y, Color.Black);
+                            resultList.Add(y.ToString() + " " + x.ToString() + " " + "0");
+                            //resultTxt += y.ToString() + " " + x.ToString() + " " + "0" + System.Environment.NewLine;
+                        }
+                    }
+                }
+
+                Bitmap managedImage = unmanagedImage.ToManagedImage();
+                pictureBox.Image = managedImage;
+            }
+        }
+
+        private void pictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (drawPoly == 1)
+            {
+                polyPoints.Add(new AForge.IntPoint(e.X, e.Y));
+                listBox1.Items.Add((e.X).ToString() + ", " + (e.Y).ToString());
+            }
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = "X: " + e.X + ", Y: " + e.Y;
+        }
+
+
+
+        private bool IsPointInPolygon(int x, int y, List<AForge.IntPoint> polygon)
+        {
+
+            PointF point = new PointF((float)x, (float)y);
+            bool isInside = false;
+
+            for (int i = 0, j = polygon.Count - 1; i < polygon.Count; j = i++)
+            {
+
+                if (((polygon[i].Y > point.Y) != (polygon[j].Y > point.Y)) &&
+
+                (point.X < (polygon[j].X - polygon[i].X) * (point.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) + polygon[i].X))
+                {
+
+                    isInside = !isInside;
+
+                }
+
+            }
+
+            return isInside;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+
+            save.FileName = fileName + ".txt";
+
+            save.Filter = "Text File | *.txt";
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+
+                StreamWriter writer = new StreamWriter(save.OpenFile());
+
+                for (int i=0; i<resultList.Count; i++)
+                    writer.WriteLine(resultList[i]);
+
+                writer.Dispose();
+
+                writer.Close();
+
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button2.PerformClick();
+        }
+
+        private void polygonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button1.PerformClick();
+        }
+
+    }
+}
